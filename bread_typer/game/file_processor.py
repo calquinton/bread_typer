547653437863c36ebdf14577f1b61c87fe2d8c80
calquinton <91569436+calquinton@users.noreply.github.com>
2021-11-30 15:01:15 -0700

    added score and score_calculator files, updated file_processor

--- bread_typer/game/file_processor.py
+++ bread_typer/game/file_processor.py
@@ -1,1 +1,32 @@
-pass
+import csv
+
+class FileProcessor:
+    '''
+    Reads csv files and returns specific lines and values
+    '''
+
+    def __init__(self):
+        pass
+
+    def get_file_line(self, file_name, line_number):
+        '''
+        Reads csv files with two values per line only
+        Returns values on a given line as a list
+        '''
+        
+        current_line = 0
+
+        # open the file
+        with open(file_name, 'r') as file:
+
+            # create a csv reader object
+            reader = csv.reader(file)
+
+            # skip the first line
+            next(file)
+
+            # read the file one line at a time
+            for line in file:
+
+                # iterate current_line by 1
+                current_line = current_line + 1
---
+++ bread_typer/game/score.py
@@ -0,0 +1,7 @@
+class Score:
+    '''
+    Creates values for players words per minute (wpm) and accuracy
+    '''
+
+    def __init__(self):
+        pass
